d63f7e7c85a74f1d3fb29012fdcd682847895f22
lingen <*\(^_^)/*>
2022-06-01 11:02:41 +0800

    编辑器和网页嵌套

--- src/main/java/org/intellij/sdk/toolWindow/MyToolWindow.java
+++ src/main/java/org/intellij/sdk/toolWindow/MyToolWindow.java
@@ -16,27 +16,45 @@ public class MyToolWindow {
     private JButton click;
     private JButton hideToolWindowButton;
     private JPanel myToolWindowContent;
+    private JButton textBtn;
 
 
     public MyToolWindow(ToolWindow toolWindow, @NotNull Project project) {
         hideToolWindowButton.addActionListener(e -> toolWindow.hide(null));
         click.addActionListener(e -> init(project));
+        textBtn.addActionListener(e -> textDo(project));
+        this.beforeCreate(project);
+    }
 
-//        this.init();
+    public void beforeCreate(@NotNull Project project) {
+
     }
 
     public void init(@NotNull Project project) {
-        if (new SampleDialogWrapper("文件选择器").showAndGet()) {
-            System.out.println("确定");
-//            文件选择
+        SampleDialogWrapper dialog = new SampleDialogWrapper("文件选择器");
+        if (dialog.showAndGet()) {
+//          文件选择
             FileChooserDescriptor fileChooserDescriptor = new FileChooserDescriptor(true, false, false, false, false, false);
             VirtualFile[] virtualFiles = FileChooser.chooseFiles(fileChooserDescriptor, project, null);
             System.out.println(virtualFiles);
+
         } else {
+
             MyNotifier.notifyError(project, "错误信息");
         }
 
     }
+
+    public void textDo(@NotNull Project project) {
+        SampleDialogWrapper dialog = new SampleDialogWrapper("信息输入");
+        if (dialog.showAndGet()) {
+            String text = dialog.getEditorTextField().getText();
+            System.out.println(text);
+
+        }
+
+    }
+
 
     public JPanel getContent() {
         return myToolWindowContent;
--- src/main/java/org/intellij/sdk/toolWindow/SampleDialogWrapper.java
+++ src/main/java/org/intellij/sdk/toolWindow/SampleDialogWrapper.java
@@ -1,6 +1,7 @@
 package org.intellij.sdk.toolWindow;
 
 import com.intellij.openapi.ui.DialogWrapper;
+import com.intellij.ui.EditorTextField;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
@@ -8,6 +9,7 @@ import java.awt.*;
 
 public class SampleDialogWrapper extends DialogWrapper {
     String showText = "";
+    EditorTextField editorTextField;
 
     public SampleDialogWrapper(String showText) {
         super(true); // use current window as parent
@@ -31,10 +33,24 @@ public class SampleDialogWrapper extends DialogWrapper {
         JLabel label = new JLabel(getShowText());
         label.setPreferredSize(new Dimension(100, 100));
         dialogPanel.add(label, BorderLayout.CENTER);
+        EditorTextField editorTextField = new EditorTextField();
+        dialogPanel.add(editorTextField, BorderLayout.SOUTH);
+
+
+        this.editorTextField = editorTextField;
+//        dialogPanel.add(new JBCefBrowser("https://www.jetbrains.com").getComponent());
         return dialogPanel;
     }
 
     public String getShowText() {
         return showText;
+    }
+
+    public EditorTextField getEditorTextField() {
+        return editorTextField;
+    }
+
+    public void setEditorTextField(EditorTextField editorTextField) {
+        this.editorTextField = editorTextField;
     }
 }
